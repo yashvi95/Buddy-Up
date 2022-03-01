97c300f959bd76b396e468573670f3016bbe8f70
Yashvi <ysiddhapura@mail.csuchico.edu>
2022-03-01 15:41:12 -0800

    Updated Login/SignUp/Connected Both

--- app/src/main/java/com/example/loginpage_main/MainActivity.java
+++ app/src/main/java/com/example/loginpage_main/MainActivity.java
@@ -2,15 +2,15 @@ package com.example.loginpage_main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.textview.MaterialTextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +20,11 @@ public class MainActivity extends AppCompatActivity {
         TextView username = (TextView) findViewById(R.id.username_text);
         TextView password = (TextView) findViewById(R.id.password_text);
 
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.LogIn_Button);
-        MaterialButton signupbtn = (MaterialButton) findViewById(R.id.SignUp_Button);
-        MaterialTextView forgotpasstxt = (MaterialTextView) findViewById(R.id.forgot_pass_text);
+        Button loginbtn = (Button) findViewById(R.id.LogIn_Button);
+        Button signupbtn = (Button) findViewById(R.id.SignUp_Button);
+        signupbtn.setOnClickListener(this);
+        TextView forgotpasstxt = (TextView) findViewById(R.id.forgot_pass_text);
+
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,12 +40,12 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        signupbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"GOING TO SIGN UP PAGE", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        signupbtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this,"GOING TO SIGN UP PAGE", Toast.LENGTH_SHORT).show();
+//            }
+//        });
 
         forgotpasstxt.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,5 +53,14 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,"GOING TO FORGOT PASSWORD PAGE", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.SignUp_Button:
+                startActivity(new Intent(this, SIgn_Up.class ));
+                break;
+        }
     }
 }
