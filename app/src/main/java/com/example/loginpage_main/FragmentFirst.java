6ffb4384bd33c18b918bf36ce925b10f7b8e4de9
roamar <rmarino4@mail.csuchico.edu>
2022-04-27 01:26:42 -0700

    Updated ListView layout for homepage with images for preferred exercises

--- app/src/main/java/com/example/loginpage_main/FragmentFirst.java
+++ app/src/main/java/com/example/loginpage_main/FragmentFirst.java
@@ -24,7 +24,6 @@ public class FragmentFirst extends Fragment {
     private View FragmentFirstView;
     private ListView list_view;
     private MyCustomListAdapter adapter1;
-    private ArrayAdapter<Information> arrayAdapter;
     private ArrayList<Information> list_of_users = new ArrayList<>();
 
     private DatabaseReference UserRef;
@@ -43,23 +42,15 @@ public class FragmentFirst extends Fragment {
         UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
 
         InitializeFields();
-        /*
-        list_view = (ListView) FragmentFirstView.findViewById(R.id.list_viewFragFirst);
-        MyCustomListAdapter adapter1 = new MyCustomListAdapter(getContext(), R.layout.my_list_item, list_of_users);
-        list_view.setAdapter(adapter1);
-         */
-        RetrieveAndDisplayUsers();
 
-//        MyCustomListAdapter adapter1 = new MyCustomListAdapter(this, R.layout.my_list_item, list_of_users)
+        RetrieveAndDisplayUsers();
 
         return FragmentFirstView;
     }
 
     private void InitializeFields() {
         list_view = (ListView) FragmentFirstView.findViewById(R.id.list_viewFragFirst);
-        //arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, list_of_users);
         adapter1 = new MyCustomListAdapter(getContext(), R.layout.my_list_item, list_of_users);
-//        list_view.setAdapter(arrayAdapter);
         list_view.setAdapter(adapter1);
     }
 
@@ -69,37 +60,6 @@ public class FragmentFirst extends Fragment {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()){
                     Information info = snapshot.getValue(Information.class);
-                    String profileInfo;
-
-                    /*
-                    if(info.getC1() == null || info.getC2() == null || info.getC3() == null)
-                    {
-                        profileInfo = info.getFirstname() + " " + info.getLastname() + "\n" + "USERNAME: " + info.getUsername() + "\nGYM: "
-                                + info.getGym() + "\n";
-                    }
-                    */
-/*
-                    profileInfo = info.getFirstname() + " " + info.getLastname() + "\n" + "USERNAME: " + info.getUsername() + "\nGYM: "
-                            + info.getGym() + "\n\nPREFERRED EXERCISES:\n";// + info.getC1() + "\n" + info.getC2() + "\n" + info.getC3() + "\n";
-
-                    if(info.getC1() != null)
-                    {
-                        profileInfo = profileInfo + info.getC1() + "\n";
-                    }
-                    if(info.getC2() != null)
-                    {
-                        profileInfo = profileInfo + info.getC2() + "\n";
-                    }
-                    if(info.getC3() != null)
-                    {
-                        profileInfo = profileInfo + info.getC3() + "\n";
-                    }
-
-                    if(info.getC1() == null && info.getC2() == null && info.getC3() == null)
-                    {
-                        profileInfo = profileInfo + "None\n";
-                    }
-*/
                     list_of_users.add(info);
                 }
                 adapter1.notifyDataSetChanged();
--- app/src/main/java/com/example/loginpage_main/MyCustomListAdapter.java
+++ app/src/main/java/com/example/loginpage_main/MyCustomListAdapter.java
@@ -36,15 +36,24 @@ public class MyCustomListAdapter extends ArrayAdapter<Information> {
         View view = inflater.inflate(R.layout.my_list_item, null);
 
         TextView textViewName = view.findViewById(R.id.homeName);
+//        TextView textViewUserName = view.findViewById(R.id.homeUserName);
         TextView textViewGym = view.findViewById(R.id.homeGym);
-        TextView textViewExercises = view.findViewById(R.id.homeExercises);
+//        TextView textViewExercises = view.findViewById(R.id.homeExercises);
         ImageView imageView = view.findViewById(R.id.homePicture);
+        ImageView c1View = view.findViewById(R.id.c1View);
+        ImageView c2View = view.findViewById(R.id.c2View);
+        ImageView c3View = view.findViewById(R.id.c3View);
+        TextView c1Text = view.findViewById(R.id.c1Text);
+        TextView c2Text = view.findViewById(R.id.c2Text);
+        TextView c3Text = view.findViewById(R.id.c3Text);
+
+
 
         Information user = userList.get(position);
 
-        textViewName.setText(user.getUsername());
+        textViewName.setText(user.getFirstname());
         textViewGym.setText("GYM: " + user.getGym());
-
+/*
         String ExerciseInfo = "Preferred Exercises:\n";
         if(user.getC1() != null)
         {
@@ -65,9 +74,130 @@ public class MyCustomListAdapter extends ArrayAdapter<Information> {
         }
 
         textViewExercises.setText(ExerciseInfo);
-
+*/
         //set to user.getImage() once it is set up.
         imageView.setImageResource(R.drawable.defaultimage);
+
+//        c1View.setImageResource(R.drawable.cardio);
+        c1Text.setText(user.getC1());
+//        c2View.setImageResource(R.drawable.yoga);
+        c2Text.setText(user.getC2());
+//        c3View.setImageResource(R.drawable.dumbell);
+        c3Text.setText(user.getC3());
+
+        switch (user.getC1().toLowerCase())
+        {
+            case "cardio":
+            {
+                c1View.setImageResource(R.drawable.cardio);
+                break;
+            }
+            case "yoga":
+            {
+                c1View.setImageResource(R.drawable.yoga);
+                break;
+            }
+            case "weight loss":
+            {
+                c1View.setImageResource(R.drawable.tape);
+                break;
+            }
+            case "strength training":
+            {
+                c1View.setImageResource(R.drawable.lift);
+                break;
+            }
+            case "body building":
+            {
+                c1View.setImageResource(R.drawable.dumbell);
+                break;
+            }
+            case "none":
+            {
+                c1View.setImageResource(R.drawable.cancel);
+                break;
+            }
+            default:
+            {
+                c1View.setImageResource(R.drawable.cancel);
+            }
+        }
+
+        switch (user.getC2().toLowerCase())
+        {
+            case "cardio":
+            {
+                c2View.setImageResource(R.drawable.cardio);
+                break;
+            }
+            case "yoga":
+            {
+                c2View.setImageResource(R.drawable.yoga);
+                break;
+            }
+            case "weight loss":
+            {
+                c2View.setImageResource(R.drawable.tape);
+                break;
+            }
+            case "strength training":
+            {
+                c2View.setImageResource(R.drawable.lift);
+                break;
+            }
+            case "body building":
+            {
+                c2View.setImageResource(R.drawable.dumbell);
+                break;
+            }
+            case "none":
+            {
+                c2View.setImageResource(R.drawable.cancel);
+                break;
+            }
+            default:
+            {
+                c2View.setImageResource(R.drawable.cancel);
+            }
+        }
+
+        switch (user.getC3().toLowerCase())
+        {
+            case "cardio":
+            {
+                c3View.setImageResource(R.drawable.cardio);
+                break;
+            }
+            case "yoga":
+            {
+                c3View.setImageResource(R.drawable.yoga);
+                break;
+            }
+            case "weight loss":
+            {
+                c3View.setImageResource(R.drawable.tape);
+                break;
+            }
+            case "strength training":
+            {
+                c3View.setImageResource(R.drawable.lift);
+                break;
+            }
+            case "body building":
+            {
+                c3View.setImageResource(R.drawable.dumbell);
+                break;
+            }
+            case "none":
+            {
+                c3View.setImageResource(R.drawable.cancel);
+                break;
+            }
+            default:
+            {
+                c3View.setImageResource(R.drawable.cancel);
+            }
+        }
 
 
         return view;
