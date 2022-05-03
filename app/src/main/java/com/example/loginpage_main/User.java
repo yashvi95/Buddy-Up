752f3e59ebbe75a6310eebdf742af94ad659f34c
Yashvi <ysiddhapura@mail.csuchico.edu>
2022-05-03 01:25:37 -0700

    Updated Signup w/Catg/Schedule/Image/Uploading to Firebase

--- app/src/main/java/com/example/loginpage_main/User.java
+++ app/src/main/java/com/example/loginpage_main/User.java
@@ -1,9 +1,9 @@
 package com.example.loginpage_main;
 
 public class User {
-    public String firstname, lastname, username, password, phonenumber, email, gym,c1,c2,c3;
+    public String firstname, lastname, username, password, phonenumber, email, gym,c1,c2,c3,schedule,image;
 
-    public User(String firstname, String lastname, String username, String password, String phonenumber, String email, String gym, String c1, String c2, String c3) {
+    public User(String firstname, String lastname, String username, String password, String phonenumber, String email, String gym, String c1, String c2, String c3, String schedule, String image) {
         this.firstname = firstname;
         this.lastname = lastname;
         this.username = username;
@@ -14,17 +14,12 @@ public class User {
         this.c1 = c1;
         this.c2 = c2;
         this.c3 = c3;
+        this.schedule = schedule;
+        this.image = image;
 
 
     }
-/*
-    public static boolean getEmail(String s) {
-        if(getEmail(s) != )
-        {
 
-        }
-    }
-*/
     public String getFirstname() {
         return firstname;
     }
